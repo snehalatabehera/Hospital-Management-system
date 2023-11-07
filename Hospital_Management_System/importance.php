94efe5bd46b34a27dc118c4d6dcf0b412d8f2de9
snehalatabehera <150181035+snehalatabehera@users.noreply.github.com>
2023-11-08 00:36:17 +0530

    Add files via upload

---
+++ Hospital_Management_System/importance.php
@@ -0,0 +1,45 @@
+<?php 
+
+// this is the important file in the system, 
+// it initializes all the classes and objects of the system
+
+
+ob_start();
+@session_start();
+
+// classes
+
+function my_autoloader($file) {
+    include 'classes/' . $file . '.class.php';
+}
+
+spl_autoload_register('my_autoloader');
+
+// Or, using an anonymous function
+spl_autoload_register(function ($file) {
+    include 'classes/' . $file . '.class.php';
+});
+// functions
+
+// get all information about this user
+
+if(User::loggedIn()){
+	$token = $_COOKIE['emr-user']; 
+	$userFirstName = User::get($token, "firstName");
+	$userSecondName = User::get($token, "secondName");
+	$userEmail = User::get($token, "email");
+	$userPassword = User::get($token, "password");
+	$userToken = User::get($token, "token");
+	$userStatus = User::get($token, "status");
+	$userPhone = User::get($token, "phone");
+	$userProfile = User::get($token, "profile");
+	$userGender = User::get($token, "gender");
+	$userRole = User::get($token, "role");
+	
+	if($userStatus == 1){
+		$userRole = "Admin";
+	} else {
+		$userRole = $userRole;
+	}
+}
+
